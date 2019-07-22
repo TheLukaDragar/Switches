0d7bd655c80123e5c218a6edb6e0d13c0a7aacc6
thelukadragar <dragarstain@gmail.com>
2019-07-22 16:32:28 +0200

    a

---
+++ app/src/main/java/com/switches/MainActivity.java
@@ -0,0 +1,31 @@
+package com.switches;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+    public void onStart() {
+
+        super.onStart();
+    }
+}
+
+
+
